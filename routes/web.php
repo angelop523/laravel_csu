f114367f04b8fad4f6d5069e094e93184025e5eb
angelop523 <73214256+angelop523@users.noreply.github.com>
2023-04-27 18:18:53 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', 'RecordController@index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [RecordController::class, 'index']);
 Route::get('/records/create', 'RecordController@create');
 Route::post('/records', 'RecordController@store');
 Route::get('/records/{id}', 'RecordController@show');
